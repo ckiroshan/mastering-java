a7fba36256ab2688ddc804962763709c7e7a3d72
ckiroshan <ckiroshan@gmail.com>
2023-06-10 22:00:40 +0530

    Added Reference Data Strings in Java.

---
+++ src/com/javabasics/Strings.java
@@ -0,0 +1,26 @@
+package com.javabasics;
+
+public class Strings {
+    public static void main(String[] args) {
+//        Strings => Reference Type
+        String channelName = "BruH!";
+        System.out.println(channelName); // Output: BruH!
+        System.out.println(channelName.length()); // Output: 5
+        System.out.println(channelName.toUpperCase()); // Output: BRUH!
+        System.out.println(channelName.toLowerCase()); // Output: bruh!
+
+        String newStringValue = "    Hey Bro!";
+        System.out.println(newStringValue); // Output:     Hey Bro!
+        System.out.println(newStringValue.trim()); // Output: Hey Bro! => removes spaces
+
+//        The following are boolean results
+        System.out.println(newStringValue.startsWith("H")); // Output: false => Starting letter is empty space
+        System.out.println(newStringValue.startsWith(" ")); // Output: true => Starting letter is empty space
+        System.out.println(newStringValue.endsWith("!")); // Output: true => Ends with '!'
+/*
+        final int constNumber = 431231; // 'final' => keyword used to declare constants.
+        System.out.println(constNumber); // Output: 431231
+
+ */
+    }
+}
