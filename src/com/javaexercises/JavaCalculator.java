a649020a971a8a5fbf62d450084f712568e67de0
ckiroshan <ckiroshan@gmail.com>
2023-06-15 22:17:28 +0530

    Basic Calculator in JAVA initiation.

---
+++ src/com/javaexercises/JavaCalculator.java
@@ -0,0 +1,15 @@
+package com.javaexercises;
+
+import java.util.Scanner;
+
+public class JavaCalculator {
+    public static void main(String[] args) {
+        System.out.println("Hey there!");
+
+//        Create a calculator
+//        Takes 2 numbers. based on user's chosen assignment operator
+//        Make changes to the numbers.
+
+
+    }
+}
