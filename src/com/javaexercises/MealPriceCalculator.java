851e6fe7a22b0981a14b6f8525cde6b005a6ed70
ckiroshan <ckiroshan@gmail.com>
2023-06-16 23:08:42 +0530

    Total bill value can be spilt among the group using a return type function.

--- src/com/javaexercises/MealPriceCalculator.java
+++ src/com/javaexercises/MealPriceCalculator.java
@@ -2,23 +2,26 @@ package com.javaexercises;
 
 public class MealPriceCalculator {
 
-    public static void calculateTotalMealPrice(double listedMealPrice) {
-        double tipRate = .12;
-        double taxRate = .10;
+    public static double calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxRate) {
         double tip = tipRate * listedMealPrice;
         double tax = taxRate * listedMealPrice;
-        double totalCost = listedMealPrice + tip + tax;
-        System.out.println("Meal Price: $" + listedMealPrice);
-        System.out.println("Tip: $" + tip);
-        System.out.println("Tax: $" + tax);
-        System.out.println("Your total price would be: $" + totalCost);
+        System.out.println("Meal price Rs:" + listedMealPrice);
+        System.out.println("Tip: Rs" + tip);
+        System.out.println("Tax: Rs" + tax);
+        return listedMealPrice + tip + tax;
     }
 
     public static void main(String[] args) {
         System.out.println("This is a Meal price calculator");
         System.out.println(" ");
 
-        calculateTotalMealPrice(22130);
+        double groupTotalMealPrice = calculateTotalMealPrice(700, .2, .08);
+        System.out.println("Total Meal price: Rs" + groupTotalMealPrice);
+
+        int eaters = 5;
+        System.out.println("Number of people in group: " + eaters);
+        double individualMealPrice = groupTotalMealPrice / eaters;
+        System.out.println("Per person cost: Rs" + individualMealPrice);
 
     }
 }
