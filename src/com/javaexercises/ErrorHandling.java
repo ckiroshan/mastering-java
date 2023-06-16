011dcff7129dfbea7ff4930da547f4a971c73190
ckiroshan <ckiroshan@gmail.com>
2023-06-16 15:42:19 +0530

    Fixed one syntax & two logical errors.

--- src/com/javaexercises/ErrorHandling.java
+++ src/com/javaexercises/ErrorHandling.java
@@ -1,6 +1,7 @@
 package com.javaexercises;
 
 import java.util.Scanner;
+
 public class ErrorHandling {
     public static void main(String[] args) {
         System.out.println("Hello World!");
@@ -13,7 +14,7 @@ public class ErrorHandling {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Please input the base of the triangle (in inches).");
-        double base = input.nextDouble()
+        double base = input.nextDouble();
 
         while (base <= 0) {
             System.out.println("That's invalid. Please input the base of the triangle (in inches).");
@@ -23,11 +24,11 @@ public class ErrorHandling {
         System.out.println("Please input the height of the triangle (in inches).");
         double height = input.nextDouble();
         while (height <= 0) {
-            System.out.println("That's invalid. Please input the base of the triangle (in inches).");
-            base = input.nextDouble();
+            System.out.println("That's invalid. Please input the height of the triangle (in inches).");
+            height = input.nextDouble();
         }
 
         double area = (base * height) / 2;
-        System.out.println("The area is " + height);
+        System.out.println("The area is " + area);
     }
 }
