f8027e67a52d26fcb6d801fe24f9ab66448bbb31
ckiroshan <ckiroshan@gmail.com>
2023-06-17 10:55:55 +0530

    A program that calculates yearly Gross salary using 3 inputs.

---
+++ src/com/javaexercises/SalaryCalculator.java
@@ -0,0 +1,37 @@
+package com.javaexercises;
+
+import java.util.Scanner;
+
+public class SalaryCalculator {
+
+    public static double calculateEmployeeSalary(double hoursPerDay, double DaysWorked, double paymentPerHour) {
+        double hoursPerWeek = hoursPerDay * DaysWorked;
+        System.out.println("Hours worked per week: " + hoursPerWeek);
+        double grossSalary = (hoursPerWeek * paymentPerHour) * 52;
+        return grossSalary;
+    }
+
+    public static void main(String[] args) {
+        /*
+         * Input 01: hours employee works per week
+         * Input 02: amount of money employee makes per hour
+         * Output: Employee gross yearly salary
+         *
+         * Bonus: Add an input that accounts for vacation days,
+         * Employee doesn't get paid on vacation days
+         * Vacation day = 8 hours of work
+         * */
+
+        System.out.println("<===== Salary Calculator =====>");
+        System.out.println(" ");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many hours do you work per day?");
+        double workingHours = input.nextDouble();
+        System.out.println("How many days do you work per week?");
+        double workingDays = input.nextDouble();
+        System.out.println("How much do you make per hour?");
+        double earnedPerHour = input.nextDouble();
+        System.out.println("Your yearly gross salary: $" + calculateEmployeeSalary(workingHours, workingDays, earnedPerHour));
+    }
+}
