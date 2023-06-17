c113cfe428a73d857322be1abdc44de6be51e6f2
ckiroshan <ckiroshan@gmail.com>
2023-06-17 18:30:14 +0530

    Numbers of sides in a triangle added as a static variable.

--- src/com/javaexercises/TriangleAreaCalculator.java
+++ src/com/javaexercises/TriangleAreaCalculator.java
@@ -2,6 +2,10 @@ package com.javaexercises;
 
 class Triangle {
     //    Defining the variables
+//    Static variables - these don't change or mutate.
+    static int numberOfSides = 3;
+
+    //    Non-static variables - these values change based on Arguments passed.
     double base;
     double height;
     double sideLenOne;
@@ -33,5 +37,6 @@ public class TriangleAreaCalculator {
         System.out.println("Area of first triangle: " + firstTriangle.findArea());
         Triangle secondTriangle = new Triangle(3, 2.598, 3, 3, 3);
         System.out.println("Area of second triangle: " + secondTriangle.findArea());
+        System.out.println("Number of sides in a triangle: " + Triangle.numberOfSides);
     }
 }
