3fad7ac95adf4cf08f9f2682564f47ead984e2d4
ckiroshan <ckiroshan@gmail.com>
2023-06-18 23:38:51 +0530

    Declaring value variables for the converter.

--- src/com/javaexercises/TemperatureConverter.java
+++ src/com/javaexercises/TemperatureConverter.java
@@ -1,10 +1,18 @@
 package com.javaexercises;
 
+import java.util.Scanner;
+
 public class TemperatureConverter {
     public static void main(String[] args) {
         System.out.println("Temperature Converter");
         System.out.println();
 
-        
+//      Declaring value variables.
+        double fahrenheit;
+        double celsius;
+        Scanner input = new Scanner(System.in);
+
+
+
     }
 }
