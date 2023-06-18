330891574b4efe1be3c1dadc21d48ba22fc93a67
ckiroshan <ckiroshan@gmail.com>
2023-06-19 00:15:09 +0530

    Added "if" block to calculate conversion.

--- src/com/javaexercises/TemperatureConverter.java
+++ src/com/javaexercises/TemperatureConverter.java
@@ -10,17 +10,29 @@ public class TemperatureConverter {
 //      Declaring value variables.
         int fahrenheit;
         int celsius;
+        String InputTemp;
         Scanner input = new Scanner(System.in);
 
 //      Get the user's input.
-        System.out.println("Enter temperature in Fahrenheit");
-        fahrenheit = input.nextInt();
+        System.out.println("Do you want to convert to F° or C°?");
+        InputTemp = input.next().toLowerCase();
+        if (InputTemp.equals("f")) {
+            System.out.println("Enter temperature in Celsius:");
+            fahrenheit = input.nextInt();
+            celsius = (fahrenheit * 9 / 5) + 32;
+            System.out.println(fahrenheit + "C° in Fahrenheit: " + celsius + "F°");
+        } else {
+            System.out.println("Enter temperature in Fahrenheit:");
+            celsius = input.nextInt();
+            fahrenheit = (celsius - 32) * 5 / 9;
+//            °F = (°C × 9/5) + 32
+            System.out.println(celsius + "F° in Celsius: " + fahrenheit + "C°");
+        }
 
 //      Convert the temperature to Celsius.
-        celsius = (fahrenheit - 32) * 5 / 9;
+
 
 //      Print the converted temperature.
-        System.out.println(fahrenheit + "F° in Celsius: " + celsius + "C°");
 
 
     }
