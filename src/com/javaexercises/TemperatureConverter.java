a2d7f04f577dd67f590d0a99358bdd98e08a5238
ckiroshan <ckiroshan@gmail.com>
2023-06-19 01:53:30 +0530

    Added "switch" block to calculate conversion & completed the program.

--- src/com/javaexercises/TemperatureConverter.java
+++ src/com/javaexercises/TemperatureConverter.java
@@ -8,32 +8,37 @@ public class TemperatureConverter {
         System.out.println();
 
 //      Declaring value variables.
-        int fahrenheit;
-        int celsius;
+        double fahrenheit;
+        double celsius;
         String InputTemp;
         Scanner input = new Scanner(System.in);
 
 //      Get the user's input.
         System.out.println("Do you want to convert to F° or C°?");
         InputTemp = input.next().toLowerCase();
-        if (InputTemp.equals("f")) {
-            System.out.println("Enter temperature in Celsius:");
-            fahrenheit = input.nextInt();
-            celsius = (fahrenheit * 9 / 5) + 32;
-            System.out.println(fahrenheit + "C° in Fahrenheit: " + celsius + "F°");
-        } else {
-            System.out.println("Enter temperature in Fahrenheit:");
-            celsius = input.nextInt();
-            fahrenheit = (celsius - 32) * 5 / 9;
-//            °F = (°C × 9/5) + 32
-            System.out.println(celsius + "F° in Celsius: " + fahrenheit + "C°");
+
+//      Switch block to check conditions.
+        switch (InputTemp) {
+            // if user chose "f"
+            case "f" -> {
+                System.out.println("Enter temperature in Celsius:");
+                celsius = input.nextInt();
+                // Convert the temperature to Fahrenheit.
+                fahrenheit = Math.floor((celsius * 1.8) + 32);
+                // Print the converted temperature.
+                System.out.println(celsius + "C° in Fahrenheit: " + fahrenheit + "F°");
+            }
+            //  if user chose "c"
+            case "c" -> {
+                System.out.println("Enter temperature in Fahrenheit:");
+                fahrenheit = input.nextInt();
+                // Convert the temperature to Celsius.
+                celsius = Math.floor((fahrenheit - 32) * 0.56);
+                // Print the converted temperature.
+                System.out.println(fahrenheit + "F° in Celsius: " + celsius + "C°");
+            }
+            default -> System.out.println("Incorrect value, please select C° or F°:");
         }
-
-//      Convert the temperature to Celsius.
-
-
-//      Print the converted temperature.
-
 
     }
 }
