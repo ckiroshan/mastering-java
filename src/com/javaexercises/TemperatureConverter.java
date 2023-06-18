061030b8db65e363462ea197a2e8380e96408cfe
ckiroshan <ckiroshan@gmail.com>
2023-06-18 23:35:31 +0530

    Initialized the Temperature converting program.

---
+++ src/com/javaexercises/TemperatureConverter.java
@@ -0,0 +1,10 @@
+package com.javaexercises;
+
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        System.out.println("Temperature Converter");
+        System.out.println();
+
+        
+    }
+}
