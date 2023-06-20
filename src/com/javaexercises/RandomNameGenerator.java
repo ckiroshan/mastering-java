212761716a32671391880a1ed41f2a5a6bc28d39
ckiroshan <ckiroshan@gmail.com>
2023-06-20 23:40:49 +0530

    Program prints one name on each iteration

--- src/com/javaexercises/RandomNameGenerator.java
+++ src/com/javaexercises/RandomNameGenerator.java
@@ -1,7 +1,18 @@
 package com.javaexercises;
 
+import java.util.Scanner;
+
 public class RandomNameGenerator {
     public static void main(String[] args) {
         System.out.println("RandomNameGenerator");
+
+        String Name[] = {"Kane", "Blake", "Richard", "Micheal"};
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Here's a random name:");
+        System.out.println(Name[0]);
+        System.out.println(Name[1]);
+        System.out.println(Name[2]);
+        System.out.println(Name[3]);
     }
 }
