8f3a262a9bedc499c65999142bddbfe4a106b7ed
Your <ckiroshan@gmail.com>
2023-08-20 01:24:11 +0530

    Polymorphism in Java.

---
+++ src/com/javaadvance/Polymorphism.java
@@ -0,0 +1,11 @@
+package com.javaadvance;
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        /*
+         * 	There are 2 types:
+         * 	Static - method overloading - compile time
+         *  Dynamic - method overriding - run time
+         * */
+    }
+}
