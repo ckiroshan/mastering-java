c9074dd1f1057cb9ebe932ac5cd4b435a73d7b33
Your <ckiroshan@gmail.com>
2023-08-21 01:28:50 +0530

    Imports & Packages in Java.

---
+++ src/com/javaadvance/ImportStatement.java
@@ -0,0 +1,12 @@
+package com.javaadvance;
+
+public class ImportStatement {
+    public static void main(String[] args) {
+        /*
+        Default/Empty: Can use everywhere except a new package.
+        Public - Can use everywhere.
+        Private - Can use only within this class.
+        Protected - Can use in any class in same package.
+         */
+    }
+}
